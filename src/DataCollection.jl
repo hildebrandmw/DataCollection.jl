14da28f644582da85c47ca033cc38d7532bc4934
Mark Hildebrand <mhildebrand@ucdavis.edu>
2020-07-10 09:19:05 -0700

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.6

---
+++ src/DataCollection.jl
@@ -0,0 +1,5 @@
+module DataCollection
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DataCollection
+using Test
+
+@testset "DataCollection.jl" begin
+    # Write your tests here.
+end
