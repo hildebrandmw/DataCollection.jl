e72447dbf78809ca36bbd05bf2c7c396a718e622
Mark Hildebrand <mhildebrand@ucdavis.edu>
2020-07-10 09:59:30 -0700

    initial commit

--- src/DataCollection.jl
+++ src/DataCollection.jl
@@ -1,5 +1,185 @@
 module DataCollection
 
-# Write your package code here.
+export AbstractParameters, AbstractData, addrow!, tokw
 
+# stdlib
+using Dates
+using Serialization
+
+# "external" dependencies
+using DataFrames
+import DataStructures: SortedDict
+
+#####
+##### Experiment Parameters
+#####
+
+# Add a hook for modifying values when we turn Parameters structs into dictionaries
+lower(x) = x
+lower(f::Function) = string(f)
+
+# Use experiment parameters to collect required information for each run.
+abstract type AbstractParameters end
+abstract type AbstractData end
+
+const PType = Union{AbstractParameters, AbstractData}
+
+kwexclude(::PType) = ()
+
+#####
+##### GenericParameters and GenericData
+#####
+
+namesof(nt::NamedTuple{names}) where {names} = names
+
+struct GenericParameters{names,T} <: AbstractParameters
+    nt::NamedTuple{names,T}
 end
+GenericParameters(; kw...) = GenericParameters((;kw...,))
+
+struct GenericData{names,T} <: AbstractData
+    nt::NamedTuple{names,T}
+end
+GenericData(; kw...) = GenericData((;kw...,))
+
+Base.propertynames(x::Union{GenericParameters,GenericData}) = namesof(x.nt)
+function Base.getproperty(x::Union{GenericParameters,GenericData}, s::Symbol)
+    nt = getfield(x, :nt)
+    s == :nt && return nt
+    return nt[s]
+end
+
+# DataFrame hooks
+dict(x::AbstractDict) = x
+dict(x::PType) = SortedDict(k => lower(getproperty(x,k)) for k in propertynames(x))
+
+dict(x...) = merge(combine_error, dict.(x)...)
+combine_error(a,b) = error("Found duplicate keys! Values are `$a` and `$b`.")
+
+# Support for turning parameters into a collection of keyword arguments
+tokw(x::PType) = (k => getproperty(x,k) for k in propertynames(x) if !in(k, kwexclude(x)))
+
+timestamp() = SortedDict(:timestamp => now())
+function addrow!(
+        df::DataFrame,
+        y::AbstractData,
+        x::Union{AbstractParameters,<:AbstractDict}...;
+        cols = :setequal,
+        force = false,
+    )
+
+    # if the datacrame is empty, then we want to set `cols` to `:union` in order
+    # to bootstrap DataFrame creation
+    d = dict(x...)
+    row = findrow(df, d)
+
+    # If a row for these parameters already exists, check if we're forcing update.
+    # If so, delete the row and continue.
+    #
+    # Otherwise, we need to throw an error
+    if !isnothing(row)
+        if force
+            delete!(df, row)
+        else
+            error("Row for parameters already exists!")
+        end
+    end
+
+    cols = isempty(df) ? :union : cols
+    push!(df, merge(timestamp(), dict(y, d)); promote = true, cols = cols)
+end
+
+# Utilities for searching for existing entries.
+struct RowSearch{T}
+    dict::T
+end
+
+RowSearch(x::AbstractParameters...) = RowSearch(dict(x...))
+
+# Treat `missing` as falses.
+unmissing(x::Bool) = x
+unmissing(x::Missing) = false
+
+function (R::RowSearch)(x)
+    pred = kv -> hasproperty(x, first(kv)) && unmissing(getproperty(x, first(kv)) == last(kv))
+    return all(pred, R.dict)
+end
+
+findrow(df::DataFrame, x::AbstractParameters...) = findrow(df, dict(x...))
+findrow(df::DataFrame, x::AbstractDict) = findrow(df, RowSearch(x))
+function findrow(df::DataFrame, f::RowSearch)
+    for (i, row) in enumerate(eachrow(df))
+        f(row) && (return i)
+    end
+    return nothing
+end
+
+hasrow(df::DataFrame, x...) = findrow(df, x...) !== nothing
+
+# Do a trick of creating a temp directory and only replacing the original when done.
+# This is something I learned a while back.
+#
+# Sometimes, serialization can error due to interrupt or for some other reason.
+#
+# If that happens and we're serializing to the same file we deserialized from, that file
+# become corrupt and we lose data - which is really sad.
+function save(df::DataFrame, path)
+    temp = tempname()
+    serialize(temp, df)
+    mv(temp, path; force = true)
+    return nothing
+end
+
+load(path::AbstractString) = ispath(path) ? deserialize(path)::DataFrame : DataFrame()
+
+#####
+##### Handle aggregates of measurements
+#####
+
+mutable struct MeasurementBundle{T, A <: NamedTuple, B <: NamedTuple}
+    measurements::T
+    pre::A
+    post::B
+end
+
+function MeasurementBundle(measurements; pre = NamedTuple(), post = NamedTuple())
+    return MeasurementBundle(measurements, pre, post)
+end
+
+# It's important that steping each of the inner iterators happens only once every time
+# we iterate on a measurement bundle in case some global monitoring is being performed
+# by some of the inner objects.
+function _iterate(m::MeasurementBundle, states)
+    # Only stop iterating when all states are empty
+    all(isnothing, states) && return nothing
+
+    # We return a named tuple that's the collection of all non-finished measurements
+    # in the `Measurements` vector.
+    #
+    # Assume that each sub-iterator returns a named tuple itself.
+    namedtuples = Any[first(s) for s in states if !isnothing(s)]
+
+    # Bookend with timestamps
+    pushfirst!(namedtuples, m.pre)
+    push!(namedtuples, m.post)
+
+    res = reduce(merge, namedtuples)
+    return res, states
+end
+
+maybeiterate(itr, state::Tuple) = iterate(itr, last(state))
+maybeiterate(itr, ::Nothing) = nothing
+
+Base.iterate(m::MeasurementBundle) = _iterate(m, iterate.(m.measurements))
+Base.iterate(m::MeasurementBundle, s) = _iterate(m, maybeiterate.(m.measurements, s))
+
+function postprocess(m::MeasurementBundle, states, args...)
+    for (measurement, state) in zip(m.measurements, states)
+        isnothing(state) && continue
+        postprocess(measurement, args...)
+    end
+    return nothing
+end
+
+end # module
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,142 @@
 using DataCollection
 using Test
 
-@testset "DataCollection.jl" begin
-    # Write your tests here.
+# For checking behavior
+using DataFrames
+import DataStructures: SortedDict
+
+struct ParametersA <: AbstractParameters
+    a::Int
+    b::Int
+end
+
+struct ParametersB <: AbstractParameters
+    c::Int
+    d::Int
+end
+
+struct ParametersC <: AbstractParameters
+    e::Int
+    f::Int
+end
+
+struct ParametersError <: AbstractParameters
+    e::Int
+    # Repeat `a` so we can test errors during merging
+    a::Int
+end
+
+struct DataA <: AbstractData
+    data::Int
+end
+
+@testset "Testing Parameters" begin
+    x = ParametersA(1,2)
+    @test DataCollection.dict(x) == SortedDict(:a => 1, :b => 2)
+
+    y = ParametersB(3,4)
+    expected = SortedDict(
+        :a => 1,
+        :b => 2,
+        :c => 3,
+        :d => 4,
+    )
+    @test DataCollection.dict(x,y) == expected
+
+    z = ParametersError(5,6)
+    @test_throws ErrorException DataCollection.dict(x,y,z)
+
+    # DataFrames schenanigans
+    df = DataFrame()
+
+    x = ParametersA(1, 2)
+    y = ParametersB(3, 4)
+    _y = ParametersB(4, 4)
+    d = DataA(10)
+
+    # Create a row for these parameters and data
+    addrow!(df, d, x, y)
+    @test nrow(df) == 1
+    @test DataCollection.hasrow(df, x, y)
+
+    # Ensure that we don't get false positives
+    @test !DataCollection.hasrow(df, x, _y)
+
+    # Get this row and make sure the data propogated properly.
+    row = df[1, :]
+    @test row.data == d.data
+
+    # Try updating.
+    @test_throws ErrorException addrow!(df, d, x, y)
+    d2 = DataA(20)
+    addrow!(df, d2, x, y; force = true)
+    row = df[1, :]
+    @test row.data == 20
+
+    # Now, add another row
+    x2 = ParametersA(10, 20)
+    y2 = ParametersB(30, 40)
+    addrow!(df, d2, x2, y2)
+    @test nrow(df) == 2
+    @test DataCollection.hasrow(df, x2, y2)
+    @test DataCollection.hasrow(df, x, y)
+
+    # Add something with different parameters.
+    # Make sure we get an error if we don't pass `cols = :union`
+    z = ParametersC(1,2)
+    @test_throws KeyError addrow!(df, d, z)
+    addrow!(df, d, z; cols = :union)
+
+    @test nrow(df) == 3
+
+    # We should still have the rows we added earlier
+    @test DataCollection.hasrow(df, x2, y2)
+    @test DataCollection.hasrow(df, x, y)
+    @test DataCollection.hasrow(df, z)
+end
+
+#####
+##### MeasurementBundle
+#####
+
+struct TestMeasurement
+    name::Symbol
+    values
+end
+
+function Base.iterate(t::TestMeasurement, s = iterate(t.values))
+    isnothing(s) && return nothing
+
+    res = NamedTuple{(t.name,)}((s[1],))
+    return res, iterate(t.values, s[2])
+end
+
+@testset "Testing MeasurementBundle" begin
+    tm1 = TestMeasurement(:test_1, [1,2,3])
+    tm2 = TestMeasurement(:test_2, [:a,:b])
+
+    bundle = DataCollection.MeasurementBundle(
+        (tm1, tm2),
+        (pre = "hello",),
+        (post = "bye",)
+    )
+
+    # First iteration
+    s = iterate(bundle)
+    @test !isnothing(s)
+    @test s[1] == (pre = "hello", test_1 = 1, test_2 = :a, post = "bye")
+
+    # Next iteration - should step through tm1 and tm2
+    s = iterate(bundle, s[2])
+    @test !isnothing(s)
+    @test s[1] == (pre = "hello", test_1 = 2, test_2 = :b, post = "bye")
+
+    # Now, tm2 should ge be dropped
+    s = iterate(bundle, s[2])
+    @test !isnothing(s)
+    @test s[1] == (pre = "hello", test_1 = 3, post = "bye")
+
+    # Final iteration, should just get nothing now
+    s = iterate(bundle, s[2])
+    @test isnothing(s)
 end
